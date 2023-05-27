7e5d9c940779e4f3ff0de3a6450e9a0291edc885
Chung Wei Leong <15154097+chungweileong94@users.noreply.github.com>
2023-05-28 01:14:15 +0800

    Support middleware (#2)
    * Support middleware

--- examples/nextjs/src/app/actions.ts
+++ examples/nextjs/src/app/actions.ts
@@ -3,14 +3,21 @@
 import {serverAct} from 'server-act';
 import {z} from 'zod';
 
+const requestTimeMiddleware = () => {
+  return {
+    requestTime: new Date(),
+  };
+};
+
 export const sayHelloAction = serverAct
+  .middleware(requestTimeMiddleware)
   .input(
     z.object({
       name: z.string().optional(),
     }),
   )
-  .action(async ({input}) => {
-    console.log('Someone say hi from the client!');
+  .action(async ({input, ctx}) => {
+    console.log(`Someone say hi from the client at ${ctx.requestTime.toTimeString()}!`);
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return input.name ? `Hello, ${input.name}!` : 'You need to tell me your name!';
   });
