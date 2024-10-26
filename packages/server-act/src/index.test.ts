40ef9669af1bebf5567557186bbfeea971f6fb0a
Chung Wei <leongchungwei@hotmail.com>
2024-10-26 18:34:56 +0800

    More test

--- packages/server-act/src/index.test.ts
+++ packages/server-act/src/index.test.ts
@@ -212,4 +212,40 @@ describe("formAction", () => {
       foo: ["Required"],
     });
   });
+
+  test("should able to access middleware context", async () => {
+    const action = serverAct
+      .middleware(() => ({ prefix: "best" }))
+      .input(({ ctx }) =>
+        zfd.formData({
+          foo: zfd.text(z.string().transform((v) => `${ctx.prefix}-${v}`)),
+        }),
+      )
+      .formAction(async ({ ctx, formErrors, input }) => {
+        if (formErrors) {
+          return formErrors;
+        }
+        return Promise.resolve(`${input.foo}-${ctx.prefix}-bar`);
+      });
+
+    type State = string | z.ZodError<{ foo: string }>;
+    expectTypeOf(action).toEqualTypeOf<
+      (
+        prevState: State | undefined,
+        formData: FormData,
+      ) => Promise<State | undefined>
+    >();
+    expectTypeOf(action).parameter(1).toHaveProperty("append");
+    expectTypeOf(action).parameter(1).toHaveProperty("delete");
+    expectTypeOf(action).parameter(1).toHaveProperty("get");
+    expectTypeOf(action).parameter(1).toHaveProperty("entries");
+
+    expect(action.constructor.name).toBe("AsyncFunction");
+
+    const formData = new FormData();
+    formData.append("foo", "bar");
+    await expect(action("foo", formData)).resolves.toMatchObject(
+      "best-bar-best-bar",
+    );
+  });
 });
